bf54426e70f1387444faefc459e4b78fa9dd8980
krolique <nick@digitalscripter.com>
2020-06-23 17:56:41 -0400

    Changes pwd prompt colors
    adjusted the colors and the pwd putput

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -9,8 +9,9 @@ set __fish_git_prompt_showcolorhints 'yes'
 # git prompt color overides
 set __fish_git_prompt_color_branch '78a6f0'
 set __fish_git_prompt_color_upstream_ahead green
-set __fish_git_prompt_color_upstream_behind red
+set __fish_git_prompt_color_upstream_behind 'dc453d'
 set __fish_git_prompt_color_cleanstate 'eb6b34'
+set __fish_git_prompt_color_invalidstate 'CC3333'
 
 # git prompt chars overrides
 set __fish_git_prompt_char_stagedstate 'Ø'
@@ -19,10 +20,14 @@ set __fish_git_prompt_char_upstream_diverged '±'
 function fish_prompt
     set last_status $status
 
-    set_color $fish_color_cwd
-    printf '%s' (prompt_pwd)
+    set_color '99FFCC'
+    printf '⋊>'
     set_color normal
   
+    set_color '009900'
+    printf ' [%s]' (pwd)
+    set_color normal
+
     printf '%s ' (__fish_git_prompt)
     set_color normal
 end
