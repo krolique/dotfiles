d1a18b3ad69bfa215c4e0824afb5de2a4e788529
krolique <nick@digitalscripter.com>
2020-06-23 17:32:37 -0400

    Updates fish_prompt
    - Changed the section comments
    - Added clean state color override

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,13 +1,18 @@
-# settings for the fish_git_prompt function. 
+# git prompt toggle settings
 set __fish_git_prompt_showdirtystate 'yes'
 set __fish_git_prompt_showupstream 'verbose'
 set __fish_git_prompt_showstashstate 'yes'
 set __fish_git_prompt_showuntrackedfiles 'yes'
 set __fish_git_prompt_show_informative_status 'yes'
 set __fish_git_prompt_showcolorhints 'yes'
+
+# git prompt color overides
 set __fish_git_prompt_color_branch '78a6f0'
 set __fish_git_prompt_color_upstream_ahead green
 set __fish_git_prompt_color_upstream_behind red
+set __fish_git_prompt_color_cleanstate 'eb6b34'
+
+# git prompt chars overrides
 set __fish_git_prompt_char_stagedstate 'Ø'
 set __fish_git_prompt_char_upstream_diverged '±'
 
