7ff7cfc7fd28faed7843a0e316c9f5aefb4e946c
krolique <nick@digitalscripter.com>
2021-10-08 19:16:10 -0400

    Fixes fish stuff
    Fixed shell prompt and brew loading

--- fish/config.fish
+++ fish/config.fish
@@ -7,6 +7,11 @@ set fish_greeting
 # install fundle if it's not present
 if not functions -q fundle; eval (curl -sfL https://git.io/fundle-install); end
 
+# load brew context (mac os)
+if status --is-interactive; and command -v /opt/homebrew/bin/brew > /dev/null;
+  eval (/opt/homebrew/bin/brew shellenv)
+end
+
 
 # Prompt before overwriting or removing a file.
 alias cp "cp -i"
